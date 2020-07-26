aa47b894ae3581d147a083cc1bb7274773a6491c
daxluna <68290954+daxluna@users.noreply.github.com>
2020-07-27 01:54:06 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The R package 'matlib' was used for this assignment for validation. It has an
+## "inv" functionfor the inversion of the matrix. It was also important to have
+## an idea aboutthe determinant of matrix to avoid errors.
 
-## Write a short comment describing this function
+## Basically, I tried to use the sample code as my based code. So expect that
+## this code would be similar.
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix or makeCaMax - I just shortened it. As mentioned, I mostly used
+## the sample code. Here 'x' was cached or stored to m.
 
+## storage function
+makeCaMax <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+      setmean = setmean,
+     getmean = getmean) 
+} 
+
+## retrieve function. After getting the matrix back. The 'mean' was generated
+## and was stored again to m.
+
+cacMean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("loading cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
 
+# start here
 
-## Write a short comment describing this function
+#install(matlib)
+library(matlib)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## The matrix was created and assigned to 'A'. This will serve as the "input".
+## The idea is like in forms, were there is space or box where inputs are placed.
+
+
+# All inputs here
+
+#A <- matrix(c(2:9,9), nrow = 3, ncol = 3)
+A <- matrix(c(4:18,12), nrow = 4, ncol = 4)
+A
+
+#B <- inv(A)
+#B
+
+# Looking at the determinant of the matrix. After some trial, the result should 
+# not be zero. An error was encountered when '0'.
+
+D <- det(A)
+print(D) 
+
+
+thematrix <- makeCaMax(A)
+cacMean (thematrix)
+cacMean (thematrix)
+
